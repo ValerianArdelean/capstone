508b6d3ccdade7a35e8ce617b52bdaa410412456
ValerianArdelean <valerian_ardelean@yahoo.com>
2020-10-15 21:30:16 +0300

    added functions to models classes

--- models.py
+++ models.py
@@ -12,7 +12,8 @@ class Providers(db.Model):
     __tablename__ = 'providers'
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(200))
-    area = db.Column(db.String())
+    services_offered = db.Column(db.String(200))
+    city = db.Column(db.String())
     adress = db.Column(db.String(150))
     phone = db.Column(db.String(30))
     website = db.Column(db.String(120))
@@ -20,6 +21,30 @@ class Providers(db.Model):
     image_link = db.Column(db.String(500))
     events = db.relationship('Events',backref='providers')
 
+    def insert(self):
+      db.session.add(self)
+      db.session.commit()
+
+    def update(self):
+      db.session.commit()
+
+    def delete(self):
+      db.session.delete(self)
+      db.session.commit()
+
+    def format(self):
+      return {
+        'id': self.id,
+        'name': self.name,
+        'services_offered':self.services_offered,
+        'city': self.city,
+        'adress': self.adress,
+        'phone': self.phone,
+        'website':self.website,
+        'social_media':self.social_media,
+        'image_link':self.image_link,
+        'events':self.events
+      }
 
 class Events(db.Model):
     id = db.Column(db.Integer,primary_key=True)
@@ -30,14 +55,58 @@ class Events(db.Model):
     customer_id = db.Column(db.Integer,db.ForeignKey('customers.id'))
     provider_id = db.Column(db.Integer,db.ForeignKey('providers.id'))
 
+    def insert(self):
+      db.session.add(self)
+      db.session.commit()
+
+    def update(self):
+      db.session.commit()
+
+    def delete(self):
+      db.session.delete(self)
+      db.session.commit()
+
+    def format(self):
+      return {
+        'id': self.id,
+        'event_name': self.event_name,
+        'event_type': self.event_type,
+        'date': self.date,
+        'rating': self.rating,
+        'customer_id':self.customer_id,
+        'provider_id':self.provider_id,
+      }
 
 class Customers(db.Model):
     __tablename__ = 'customers'
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(200))
-    area = db.Column(db.String)
+    city = db.Column(db.String)
     adress = db.Column(db.String(150))
     phone = db.Column(db.String(30))
     social_media = db.Column(db.String(100))
     image_link = db.Column(db.String(500))
     events = db.relationship('Events',backref='customers')
+
+    def insert(self):
+      db.session.add(self)
+      db.session.commit()
+
+    def update(self):
+      db.session.commit()
+
+    def delete(self):
+      db.session.delete(self)
+      db.session.commit()
+
+    def format(self):
+      return {
+        'id': self.id,
+        'name': self.name,
+        'city': self.city,
+        'adress': self.adress,
+        'phone': self.phone,
+        'social_media':self.social_media,
+        'image_link':self.image_link,
+        'events':self.events
+      }
