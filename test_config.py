9084345e3e2343017a0aefe26fb7ca0f865f7e88
ValerianArdelean <valerian_ardelean@yahoo.com>
2020-11-08 21:40:39 +0200

    pycodestyle file

--- test_config.py
+++ test_config.py
@@ -8,6 +8,9 @@ from models import Providers, Customers, setup_db
 from auth import AuthError, requires_auth
 
 
+'''define the test class '''
+
+
 class TestProviders(unittest.TestCase):
 
     @classmethod
@@ -19,37 +22,41 @@ class TestProviders(unittest.TestCase):
         pass
 
     def setUp(self):
+        '''initialize app and test client '''
         self.app = app
         self.client = self.app.test_client
         self.database_path = 'postgresql://valerian@localhost:5432/capstone-test'
         setup_db(self.app, self.database_path)
 
+        '''initialize providers resources tests'''
         self.provider_token = os.environ['PROVIDER_TOKEN']
         self.provider_id = os.environ['provider_id']
         self.provider_header = {'Content-Type': 'application/json',
                                 'Authorization': 'Bearer {}'.format(self.provider_token)}
-        self.new_provider = {'name': 'NAME', 'services_offered':'services',
-                             'city': 'CITY','adress': 'ADDRESS',
-                             'phone': 'PHONE', 'website':'website',
+        self.new_provider = {'name': 'NAME', 'services_offered': 'services',
+                             'city': 'CITY', 'adress': 'ADDRESS',
+                             'phone': 'PHONE', 'website': 'website',
                              'social_media': 'facemol', 'image_link': 'immage'}
 
+        '''initialize customers resources tests'''
         self.customer_token = os.environ['CUSTOMER_TOKEN']
         self.customer_id = os.environ['customer_id']
         self.customer_header = {'Content-Type': 'application/json',
                                 'Authorization': 'Bearer {}'.format(self.customer_token)}
-        self.new_customer = {'name': 'NAME', 'city': 'CITY','adress': 'ADDRESS',
+        self.new_customer = {'name': 'NAME', 'city': 'CITY', 'adress': 'ADDRESS',
                              'phone': 'PHONE', 'social_media': 'facemol',
                              'image_link': 'immage'}
 
+        '''initialize events resources tests'''
         self.events_token = self.provider_token
         self.event_id = os.environ['event_id']
-        self.new_event = {'event_name':'NAME', 'event_type': 'photo-video',
-                          'date':'2020-03-05', 'rating': 9}
+        self.new_event = {'event_name': 'NAME', 'event_type': 'photo-video',
+                          'date': '2020-03-05', 'rating': 9}
 
         with self.app.app_context():
-             self.db = SQLAlchemy()
-             self.db.init_app(self.app)
-             self.db.create_all()
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            self.db.create_all()
 
     def tearDown(self):
         pass
@@ -61,7 +68,25 @@ class TestProviders(unittest.TestCase):
         res2 = self.client().get('/aur')
         self.assertEqual(res2.status_code, 404)
 
-    '''Test get all providers routes'''
+    '''TESTING PROVIDERS RESOURCES'''
+    '''Tests post a specific provider'''
+    def test_200_post_provider(self):
+        res = self.client().post('/providers', headers={'Content-Type': 'application/json',
+                                                        'Authorization': 'Bearer {}'.format(self.provider_token)},
+                                               json=self.new_provider)
+        data = json.loads(res.data)
+        print('inserted provider id', data['id'])
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_403_post_provider(self):
+        res = self.client().post('/providers', headers={'Content-Type': 'application/json',
+                                                        'Authorization': 'Bearer {}'.format(self.customer_token)},
+                                               json=self.new_provider)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 403)
+
+    '''Tests get all providers'''
     def test_200_get_providers(self):
         res = self.client().get('/providers')
         data = json.loads(res.data)
@@ -75,20 +100,20 @@ class TestProviders(unittest.TestCase):
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
-    '''Test search for a provider'''
+    '''Tests search for a provider'''
     def test_200_search_providers(self):
-        res = self.client().patch('/providers',json={'searched_term':'are'})
+        res = self.client().patch('/providers', json={'searched_term': 'are'})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
     def test_400_search_providers(self):
-        res = self.client().patch('/providers',json={'searched_MISSPEELL':'are'})
+        res = self.client().patch('/providers', json={'searched_MISSPEELL': 'are'})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 400)
         self.assertEqual(data['success'], False)
 
-    '''Test get for a specific provider'''
+    '''Tests get for a specific provider'''
     def test_200_get_provider(self):
         res = self.client().get('/providers/1')
         data = json.loads(res.data)
@@ -101,59 +126,57 @@ class TestProviders(unittest.TestCase):
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
-    def test_200_post_provider(self):
-        res = self.client().post('/providers', headers = {'Content-Type': 'application/json',
-                                    'Authorization': 'Bearer {}'.format(self.provider_token)},
-                                    json = self.new_provider)
-        data = json.loads(res.data)
-        print('inserted provider id',data['id'])
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(data['success'], True)
-
-    def test_403_post_provider(self):
-        res = self.client().post('/providers', headers = {'Content-Type': 'application/json',
-                                    'Authorization': 'Bearer {}'.format(self.customer_token)},
-                                    json = self.new_provider)
-        data = json.loads(res.data)
-        self.assertEqual(res.status_code, 403)
-
+    '''Test edit a specific provider'''
     def test_200_edit_provider(self):
-        res = self.client().patch('/providers/1', headers = {'Content-Type': 'application/json',
-                                    'Authorization': 'Bearer {}'.format(self.provider_token)},
-                                    json = {'name': 'EDITED NAME', 'city': 'EDIT CITY'})
+        res = self.client().patch('/providers/1', headers={'Content-Type': 'application/json',
+                                                           'Authorization': 'Bearer {}'.format(self.provider_token)},
+                                                  json={'name': 'EDITED NAME', 'city': 'EDIT CITY'})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
     def test_401_edit_provider(self):
-        res = self.client().patch('/providers/1', headers = {'Content-Type': 'application/json'},
-                                    json = {'name': 'EDITED NAME', 'city': 'EDIT CITY'})
+        res = self.client().patch('/providers/1', headers={'Content-Type': 'application/json'},
+                                                  json={'name': 'EDITED NAME', 'city': 'EDIT CITY'})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 401)
 
-
+    '''Test delete for a specific provider'''
     def test_200_delete_provider(self):
-        res = self.client().delete('/providers/{}'.format(self.provider_id), headers = {'Content-Type': 'application/json',
-                                    'Authorization': 'Bearer {}'.format(self.provider_token)})
+        res = self.client().delete('/providers/{}'.format(self.provider_id),
+                                        headers={'Content-Type': 'application/json',
+                                                 'Authorization': 'Bearer {}'.format(self.provider_token)})
         data = json.loads(res.data)
         print(data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
     def test_404_delete_provider(self):
-        res = self.client().delete('/providers/203', headers = {'Content-Type': 'application/json',
-                                    'Authorization': 'Bearer {}'.format(self.provider_token)})
+        res = self.client().delete('/providers/203', headers={'Content-Type': 'application/json',
+                                                              'Authorization': 'Bearer {}'.format(self.provider_token)})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
+    '''TESTING CUSTOMERS RESOURCES'''
+    '''Tests post a customer'''
+    def test_200_post_customers(self):
+        res = self.client().post('/customers', headers={'Content-Type': 'application/json',
+                                                        'Authorization': 'Bearer {}'.format(self.customer_token)},
+                                               json=self.new_customer)
+        data = json.loads(res.data)
+        print('inserted customer id', data['id'])
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
 
+    def test_403_post_customer(self):
+        res = self.client().post('/customers', headers={'Content-Type': 'application/json',
+                                                      'Authorization': 'Bearer {}'.format(self.provider_token)},
+                                             json=self.new_provider)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 403)
 
-
-
-
-
-    '''Test get all customers routes'''
+    '''Tests get all customers routes'''
     def test_200_get_customers(self):
         res = self.client().get('/customers')
         data = json.loads(res.data)
@@ -167,20 +190,20 @@ class TestProviders(unittest.TestCase):
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
-    '''Test search for a customer'''
+    '''Tests search for a customer'''
     def test_200_search_customers(self):
-        res = self.client().patch('/customers',json={'searched_term':'are'})
+        res = self.client().patch('/customers', json={'searched_term': 'are'})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
     def test_400_search_customers(self):
-        res = self.client().patch('/customers',json={'searched_MISSPEELL':'are'})
+        res = self.client().patch('/customers', json={'searched_MISSPEELL': 'are'})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 400)
         self.assertEqual(data['success'], False)
 
-    '''Test get for a specific customer'''
+    '''Tests get for a specific customer'''
     def test_200_get_customer(self):
         res = self.client().get('/customers/1')
         data = json.loads(res.data)
@@ -193,58 +216,57 @@ class TestProviders(unittest.TestCase):
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
-    def test_200_post_customers(self):
-        res = self.client().post('/customers', headers = {'Content-Type': 'application/json',
-                                 'Authorization': 'Bearer {}'.format(self.customer_token)},
-                                 json = self.new_customer)
-        data = json.loads(res.data)
-        print('inserted customer id', data['id'])
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(data['success'], True)
-
-    def test_403_post_customer(self):
-        res = self.client().post('/customers', headers = {'Content-Type': 'application/json',
-                                    'Authorization': 'Bearer {}'.format(self.provider_token)},
-                                    json = self.new_provider)
-        data = json.loads(res.data)
-        self.assertEqual(res.status_code, 403)
-
+    '''Tests edit a customer'''
     def test_200_edit_customer(self):
-        res = self.client().patch('/customers/1', headers = {'Content-Type': 'application/json',
-                                    'Authorization': 'Bearer {}'.format(self.customer_token)},
-                                    json = {'name': 'EDITED NAME', 'city': 'EDIT CITY'})
+        res = self.client().patch('/customers/1', headers={'Content-Type': 'application/json',
+                                                           'Authorization': 'Bearer {}'.format(self.customer_token)},
+                                                  json={'name': 'EDITED NAME', 'city': 'EDIT CITY'})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
     def test_401_edit_customer(self):
-        res = self.client().patch('/customers/1', headers = {'Content-Type': 'application/json'},
-                                    json = {'name': 'EDITED NAME', 'city': 'EDIT CITY'})
+        res = self.client().patch('/customers/1', headers={'Content-Type': 'application/json'},
+                                                  json={'name': 'EDITED NAME', 'city': 'EDIT CITY'})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 401)
 
+    '''Tests delete a customer'''
     def test_200_delete_customer(self):
-        res = self.client().delete('/customers/{}'.format(self.customer_id), headers = {'Content-Type': 'application/json',
-                                    'Authorization': 'Bearer {}'.format(self.customer_token)})
+        res = self.client().delete('/customers/{}'.format(self.customer_id),
+                                        headers={'Content-Type': 'application/json',
+                                                 'Authorization': 'Bearer {}'.format(self.customer_token)})
         data = json.loads(res.data)
         print(data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
     def test_404_delete_customer(self):
-        res = self.client().delete('/customers/203', headers = {'Content-Type': 'application/json',
-                                    'Authorization': 'Bearer {}'.format(self.customer_token)})
+        res = self.client().delete('/customers/203', headers={'Content-Type': 'application/json',
+                                                              'Authorization': 'Bearer {}'.format(self.customer_token)})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
+    '''TESTING EVENTS RESOURCES'''
+    '''Tests post a event'''
+    def test_200_post_events(self):
+        res = self.client().post('/events', headers={'Content-Type': 'application/json',
+                                                     'Authorization': 'Bearer {}'.format(self.events_token)},
+                                            json=self.new_event)
+        data = json.loads(res.data)
+        print('inserted events id', data['id'])
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
 
+    def test_noBEARRER_post_events(self):
+        res = self.client().post('/events', headers={'Content-Type': 'application/json',
+                                                     'Authorization': '{}'.format(self.events_token)},
+                                            json=self.new_event)
+        data = json.loads(res.data)
+        self.assertEqual(data['code'], 'invalid_header')
 
-
-
-
-
-    '''Test get all customers routes'''
+    '''Tests get all events'''
     def test_get_events(self):
         res = self.client().get('/events')
         data = json.loads(res.data)
@@ -258,49 +280,37 @@ class TestProviders(unittest.TestCase):
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
-    def test_200_post_events(self):
-        res = self.client().post('/events', headers = {'Content-Type': 'application/json',
-                                 'Authorization': 'Bearer {}'.format(self.events_token)},
-                                 json = self.new_event)
-        data = json.loads(res.data)
-        print('inserted events id', data['id'])
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(data['success'], True)
-
-    def test_noBEARRER_post_events(self):
-        res = self.client().post('/events', headers = {'Content-Type': 'application/json',
-                                 'Authorization': '{}'.format(self.events_token)},
-                                 json = self.new_event)
-        data = json.loads(res.data)
-        self.assertEqual(data['code'], 'invalid_header')
-
+    '''Tests edit a event'''
     def test_200_edit_events(self):
-        res = self.client().patch('/events/1', headers = {'Content-Type': 'application/json',
-                                    'Authorization': 'Bearer {}'.format(self.events_token)},
-                                    json = {'event_name': 'EDITED NAME', 'rating': 0000})
+        res = self.client().patch('/events/1', headers={'Content-Type': 'application/json',
+                                                      'Authorization': 'Bearer {}'.format(self.events_token)},
+                                             json={'event_name': 'EDITED NAME', 'rating': 0000})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
     def test_400_edit_events(self):
-        res = self.client().patch('/events/1', headers = {'Content-Type': 'application/json',
-                                    'Authorization': 'Bearer {}'.format(self.events_token)},
-                                    json = {'MISSPEL_name': 'EDITED NAME', 'rating': 'WRONG DATA'})
+        res = self.client().patch('/events/1', headers={'Content-Type': 'application/json',
+                                                        'Authorization': 'Bearer {}'.format(self.events_token)},
+                                               json={'MISSPEL_name': 'EDITED NAME',
+                                                     'rating': 'WRONG DATA'})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 400)
         self.assertEqual(data['success'], False)
 
+    '''Tests delete a event'''
     def test_200_delete_events(self):
-        res = self.client().delete('/events/{}'.format(self.event_id), headers = {'Content-Type': 'application/json',
-                                    'Authorization': 'Bearer {}'.format(self.events_token)})
+        res = self.client().delete('/events/{}'.format(self.event_id),
+                                    headers={'Content-Type': 'application/json',
+                                               'Authorization': 'Bearer {}'.format(self.events_token)})
         data = json.loads(res.data)
         print(data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
     def test_404_delete_events(self):
-        res = self.client().delete('/events/203', headers = {'Content-Type': 'application/json',
-                                    'Authorization': 'Bearer {}'.format(self.events_token)})
+        res = self.client().delete('/events/203', headers={'Content-Type': 'application/json',
+                                                           'Authorization': 'Bearer {}'.format(self.events_token)})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
